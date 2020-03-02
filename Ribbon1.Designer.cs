39979ef16657f2451164f64ff11716e754ff87ba
NansenZhao <happyzhaonan@live.com>
2020-03-02 11:42:45 +0800

    添加项目文件。

---
+++ Ribbon1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace WordAddIn1
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Microsoft.Office.Tools.Ribbon.RibbonDialogLauncher ribbonDialogLauncherImpl1 = this.Factory.CreateRibbonDialogLauncher();
+            this.myTest = this.Factory.CreateRibbonTab();
+            this.myGroup = this.Factory.CreateRibbonGroup();
+            this.myTest.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // myTest
+            // 
+            this.myTest.Groups.Add(this.myGroup);
+            this.myTest.Label = "测试标签";
+            this.myTest.Name = "myTest";
+            // 
+            // myGroup
+            // 
+            this.myGroup.DialogLauncher = ribbonDialogLauncherImpl1;
+            this.myGroup.Label = "测试分组";
+            this.myGroup.Name = "myGroup";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.myTest);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.myTest.ResumeLayout(false);
+            this.myTest.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab myTest;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup myGroup;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
